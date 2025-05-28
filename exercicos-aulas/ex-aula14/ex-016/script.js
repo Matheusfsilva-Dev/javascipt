98108d6bb71c53d192e498f3ba6bcd8cb602a829
Matheus Interaminense Ferreira Da Silva <ferreira074matheus@gmail.com>
2025-05-28 05:22:47 -0300

    criei o exercício 017

--- exercicos-aulas/ex-aula14/ex-016/script.js
+++ exercicos-aulas/ex-aula14/ex-016/script.js
@@ -5,15 +5,20 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!')
+        res.innerHTML =  `Impossivel contar!`
+        //alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'contando: '
+        res.innerHTML = 'contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0 ) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             //contagem crescente
-            for(let c = i; c <=f; c+=p) {
+            for(let c = i; c <= f; c += p) {
                 res.innerHTML += ` ${c} \u{1F449}`
             }
         }else {
